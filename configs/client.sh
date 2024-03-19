2ce49cc2e6ea5f75af40c4ef991248417b0f1e35
Roman Dodin <dodin.roman@gmail.com>
2024-03-20 01:24:13 +0200

    added dc2 configs and client routes

--- configs/client.sh
+++ configs/client.sh
@@ -6,8 +6,10 @@ ID=$2
 
 if [ "$DC_ID" = "dc1" ]; then
     FIRST_OCTET=10
+    REMOTE_OCTET=20
 elif [ "$DC_ID" = "dc2" ]; then
     FIRST_OCTET=20
+    REMOTE_OCTET=10
 else
     echo "Invalid DC_ID"
 fi
@@ -16,10 +18,15 @@ fi
 ip addr add ${FIRST_OCTET}.0.0.${ID}/24 dev eth1
 ip link set dev eth1 address aa:c1:ab:${FIRST_OCTET}:00:0${ID}
 
-# a route towards the anycast gw
+# a route towards the local DC clients via the anycast gw
 ip route add ${FIRST_OCTET}.0.0.0/24 via ${FIRST_OCTET}.0.0.254
 # a route towards the leaf loopbacks in the l3 vrf
 ip route add ${FIRST_OCTET}.1.0.0/24 via ${FIRST_OCTET}.0.0.254
+
+# a route towards the remote DC clients via the anycast gw
+ip route add ${REMOTE_OCTET}.0.0.0/24 via ${FIRST_OCTET}.0.0.254
+# a route towards the remote leafs loopbacks in the l3 vrf
+ip route add ${REMOTE_OCTET}.1.0.0/24 via ${FIRST_OCTET}.0.0.254
 
 # set PS1
 echo "export PS1='\[\033[0;32m\]\u@\h:\[\033[36m\]\W\[\033[0m\] \$ '" > /root/.bashrc
