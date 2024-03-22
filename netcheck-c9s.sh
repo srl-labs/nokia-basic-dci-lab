78f0eb4212e48ee783cd90811c17c21e8aa0abb2
Roman Dodin <dodin.roman@gmail.com>
2024-03-22 12:00:47 +0200

    improvements to the pinger script

--- netcheck-c9s.sh
+++ netcheck-c9s.sh
@@ -14,9 +14,9 @@ if [ -z "$CASE" ]; then
 fi
 
 ping_test() {
-    kubectl exec -it -n $NS $POD -- docker exec -it $POD_SHORTNAME ping -c 1 -w 1 $1 > /dev/null
+    kubectl exec -it -n $NS $POD -- docker exec -it $POD_SHORTNAME ping -c 1 -w 2 $1 &> /dev/null
     if [ $? -eq 0 ]; then
-        echo -e "+ \e[32mPing from client1 (dc1) to $2 ($1) was successful.\e[0m"
+        echo -e "+ \e[32mPing from client1/dc1 to $2 ($1) was successful.\e[0m"
     else
         echo -e "- \e[31mPing to $1 ($2) failed.\e[0m"
     fi
@@ -37,12 +37,12 @@ if [ "$CASE" == "dc2" ]; then
 fi
 
 if [ "$CASE" == "all" ]; then
-    echo "Running pings from leaf1 (dc1)"
+    echo "Running pings from client1/dc1 (10.0.0.1)"
     # ping all cases and print the results that pass and don't pass
-    ping_test 10.0.0.4 "client2 (dc1)"
+    ping_test 10.0.0.4 "client2/dc1"
     echo
-    ping_test 20.0.0.1 "client3 (dc2)"
-    ping_test 20.0.0.4 "client4 (dc2)"
+    ping_test 20.0.0.1 "client3/dc2"
+    ping_test 20.0.0.4 "client4/dc2"
     exit 0
 fi
 
